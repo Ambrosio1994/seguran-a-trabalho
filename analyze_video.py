214a196ec15245a7c33cbf83233e0a9f89ad358b
Ambrosio <sgtambrosio2019@gmail.com>
2025-04-01 16:16:32 -0300

    atualizacao de funcao

--- analyze_video.py
+++ analyze_video.py
@@ -7,33 +7,35 @@ import os
 load_dotenv()
 
 apiKey = os.getenv('GEMINI_API_KEY')
-video_file_name = "C:\\Users\\diham\\segurança-trabalho\\project\\video.mp4"
-display_name = "sampleDisplayName"
 
 genai.configure(api_key=apiKey)
 
 fileList = genai.list_files(page_size=100)
 
 # Check uploaded file.
-video_file = next((f for f in fileList if f.display_name == display_name), None)
-if video_file is None:
-    print(f"Uploading file...")
-    video_file = genai.upload_file(path=video_file_name, display_name=display_name, resumable=True)
-    print(f"Completed upload: {video_file.uri}")
-else:
-    print(f"File URI: {video_file.uri}")
+def analyze_video(video_file_name:str, 
+                  display_name:str="video.mp4",
+                  model_name:str="gemini-2.0-flash"):
+    
+    video_file = next((f for f in fileList if f.display_name == display_name), None)
+    if video_file is None:
+        print(f"Uploading file...")
+        video_file = genai.upload_file(path=video_file_name, display_name=display_name, resumable=True)
+        print(f"Completed upload: {video_file.uri}")
+    else:
+        print(f"File URI: {video_file.uri}")
 
-# Check the state of the uploaded file.
-print("Processing file...")
-while video_file.state.name == "PROCESSING":
-    time.sleep(5)
-    video_file = genai.get_file(video_file.name)
+    # Check the state of the uploaded file.
+    print("Processing file...")
+    while video_file.state.name == "PROCESSING":
+        time.sleep(5)
+        video_file = genai.get_file(video_file.name)
 
-if video_file.state.name == "FAILED":
-    raise ValueError(video_file.state.name)
+    if video_file.state.name == "FAILED":
+        raise ValueError(video_file.state.name)
 
-# Generate content using the uploaded file.
-model = genai.GenerativeModel(model_name="models/gemini-2.0-flash")
-print("Making LLM inference request...")
-response = model.generate_content([video_file, prompt_video], request_options={"timeout": 600})
-print(response.text)
+    # Generate content using the uploaded file.
+    model = genai.GenerativeModel(model_name=f'models/{model_name}')
+    print("Making LLM inference request...")
+    response = model.generate_content([video_file, prompt_video], request_options={"timeout": 600})
+    return response.text
