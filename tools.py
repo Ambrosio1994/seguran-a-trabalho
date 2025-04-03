ec22d0114aaf2d400e4d752131b041503a5af384
Ambrosio <sgtambrosio2019@gmail.com>
2025-04-03 18:52:45 -0300

    docstring

--- tools.py
+++ tools.py
@@ -1,22 +1,35 @@
 from langchain_core.tools import tool
 import pandas as pd
 import os
-
+"""
+Esta função adiciona novos registros de riscos ocupacionais a um arquivo CSV existente. 
+Ela é decorada com @tool da LangChain para ser usada como uma ferramenta dentro de aplicações 
+que utilizam esse framework.
+"""
+# Função para adicionar dados ao DataFrame
 @tool
 def add_data_to_df(
     risco: str,
-    fonte_geradora:str,
+    fonte_geradora: str,
     agente: str,
     medidas_de_controle: str,
     severidade: str,
     probabilidade: str,
-    nivel_de_Risco: str
+    nivel_de_risco: str
     ) -> str:
-    
-    """Adiciona dados ao dataframe"""
+    """Adiciona dados ao dataframe
+      Parameters:
+      risco (str): Potencial de dano de cada risco
+      fonte_geradora (str): Origem ou fonte que gera o risco no ambiente, qual material ou produto que está gerando o risco.
+      agente (str): Qual tipo do risco (ex: Físico, Químico, Biológico, Acidente, Ergonômico, Psicossociais, etc.).
+      medidas_de_controle (str): Ações preventivas ou corretivas para mitigar o risco.
+      severidade (str): Nível de gravidade do risco (ex: IRREVERSÍVEL SEVERO, LEVE, REVERSÍVEL SEVERO, INCAPACITANTE OU FATAL, ALTAMENTE CATASTRÓFICO).
+      probabilidade (str): Chance de ocorrência do risco (ex: IMPROVÁVEL, POSSÍVEL, MUITO IMPROVÁVEL, MUITO PROVÁVEL, PROVÁVEL).
+      nivel_de_risco (str): Classificação geral resultante da combinação de severidade e probabilidade (ex: RISCO MÉDIO, RISCO BAIXO, RISCO IRRELEVANTE, RISCO ALTO, RISCO CRÍTICO)
+    """
     path_data = os.path.join(os.path.dirname(__file__), "data.csv")
-    
-    # Specify encoding when reading the CSV
+
+    # Ler o CSV existente
     df = pd.read_csv(path_data)
 
     # Criar um dicionário com os dados
@@ -27,7 +40,7 @@ def add_data_to_df(
         "Medidas_de_Controle": medidas_de_controle,
         "Severidade": severidade,
         "Probabilidade": probabilidade,
-        "Nivel_de_Risco": nivel_de_Risco
+        "Nivel_de_Risco": nivel_de_risco
     }
 
     # Adicionar os dados ao dataframe
@@ -36,7 +49,4 @@ def add_data_to_df(
     # Salvar o dataframe
     df.to_csv(path_data, index=False)
 
-    # Salvar o dataframe
-    return "Dados adicionados com sucesso!"
-
-tools = [add_data_to_df]
+    return f"Dados adicionados com sucesso! {new_data}"
