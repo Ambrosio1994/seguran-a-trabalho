0f988b61437f335897e55a49a26be6a6bbdcae72
Ambrosio1994 <sgtambrosio2019@gmail.com>
2025-04-26 00:07:47 -0300

    atualizacao de modelo 4o

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 from agents import Agent, Runner
-from tools import add_data_to_df
+from dotenv import load_dotenv
+
+from tools import tools
 from analyze_video import analyze_video
 from prompts import PROMPT_SYS_MESSAGE
-import os
-import asyncio
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
 agent_csv = Agent(
     name="assistant_csv",
+    model="gpt-4o",
     instructions=PROMPT_SYS_MESSAGE,
-    tools=[add_data_to_df],
+    tools=tools,
     )
 
 async def assistant(path_video):
     analysis = analyze_video(path_video)
     result = await Runner.run(agent_csv, analysis)
     return result
-
-if __name__ == "__main__":
-    path_video = os.path.join(os.path.dirname(__file__), "video.mp4")
-    result = asyncio.run(assistant(path_video))
-    print(result)
